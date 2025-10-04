8fcc6380ced5473645ec76c6142667630df4beb7
fkeyz <fkeyzuwu@gmail.com>
2025-10-05 00:00:02 +0300

    okay garbage can!! stuff

--- item.gd
+++ item.gd
@@ -1,5 +1,6 @@
 class_name Item extends Resource
 
-@export var item_name: String
+@export var item_name: String = "default"
+@export_multiline var description: String
 @export var scene: PackedScene
-@export var components: Array[Item]
+@export var components: Array[Item] = [null, null]
