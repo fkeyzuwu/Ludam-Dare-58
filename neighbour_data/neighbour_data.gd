41dccff97bfb194f8a6bbbe16e821fdd97593832
fkeyz <fkeyzuwu@gmail.com>
2025-10-05 00:42:56 +0300

    okayyy

--- neighbour_data/neighbour_data.gd
+++ neighbour_data/neighbour_data.gd
@@ -3,6 +3,6 @@ class_name NeighbourData extends Resource
 @export var mesh_data: NeighbourMeshData
 @export var neighbour_name: String = "default"
 @export var color: Color = Color.WHITE
-@export var throw_objects: Array[Item]
-@export var wanted_items: Array[Item]
+@export var throw_objects: Array[Item] = []
+@export var wanted_items: Array[Item] = []
 @export var dialogues: Array[DialogueData] = [DialogueData.new()]
