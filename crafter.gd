892b35446506efc37b0a6eac4f1f1f452f1b219b
fkeyz <fkeyzuwu@gmail.com>
2025-10-05 17:41:38 +0300

    okay more stuiff crafting wait

---
+++ crafter.gd
@@ -0,0 +1,1 @@
+class_name Crafter extends VBoxContainer
--- player.gd
+++ player.gd
@@ -19,23 +19,43 @@ func _ready() -> void:
 	hud.dialogue_box.opened.connect(_on_dialogue_box_opened)
 	hud.dialogue_box.closed.connect(_on_dialogue_box_closed)
 
-var in_dialogue := false
+enum State {
+	Idle,
+	Dialogue,
+	Crafting
+}
+
+var state := State.Idle
+
+func enter_state(_state: State) -> void:
+	
+	match _state:
+		State.Idle:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		State.Dialogue:
+			hud.hide_interaction_text()
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		State.Crafting:
+			hud.hide_interaction_text()
+			hud.show_inventory_crafting()
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		
+	state = _state
 
 func _on_dialogue_box_opened() -> void:
-	in_dialogue = true
-	hud.hide_interaction_text()
+	enter_state(State.Dialogue)
 	
 func _on_dialogue_box_closed() -> void:
-	in_dialogue = false
+	enter_state(State.Idle)
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion and not in_dialogue:
+	if event is InputEventMouseMotion and state == State.Idle:
 		var delta = get_process_delta_time()
 		global_rotation.y -= event.relative.x * delta * mouse_sensitivity
 		camera.global_rotation.x -= event.relative.y * delta * mouse_sensitivity
 		camera.global_rotation_degrees.x = clampf(camera.global_rotation_degrees.x, -85, 85)
-	elif event.is_action_pressed(&"craft"):
-		inventory.craft_item(inventory.items)
+	elif event.is_action_pressed(&"craft") and state == State.Idle:
+		enter_state(State.Crafting) #inventory.craft_item(inventory.items)
 	elif event.is_action_pressed(&"change_mouse_mode"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED else Input.MOUSE_MODE_CAPTURED
 	elif event.is_action_pressed("quit"):
@@ -49,12 +69,12 @@ func move(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	if Input.is_action_just_pressed("jump") and is_on_floor() and not in_dialogue:
+	if Input.is_action_just_pressed("jump") and is_on_floor() and state == State.Idle:
 		velocity.y = JUMP_VELOCITY
 
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction and not in_dialogue:
+	if direction and state == State.Idle:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
@@ -64,24 +84,25 @@ func move(delta: float) -> void:
 	move_and_slide()
 
 func try_interact() -> void:
-	if not in_dialogue:
-		if interaction_raycast.is_colliding():
-			var interactable = interaction_raycast.get_collider()
-			if not interactable:
-				hud.hide_interaction_text()
-			else:
-				if interactable.can_interact():
-					if Input.is_action_just_pressed(&"interact"):
-						await interactable.interact(self)
-					else:
-						hud.show_interaction_text(interactable.get_interaction_text())
-				else:
+	match state:
+		State.Idle:
+			if interaction_raycast.is_colliding():
+				var interactable = interaction_raycast.get_collider()
+				if not interactable:
 					hud.hide_interaction_text()
-		else:
-			hud.hide_interaction_text()
-	else:
-		if Input.is_action_just_pressed(&"interact"):
-			hud.dialogue_box.continue_dialogue()
+				else:
+					if interactable.can_interact():
+						if Input.is_action_just_pressed(&"interact"):
+							await interactable.interact(self)
+						else:
+							hud.show_interaction_text(interactable.get_interaction_text())
+					else:
+						hud.hide_interaction_text()
+			else:
+				hud.hide_interaction_text()
+		State.Dialogue:
+			if Input.is_action_just_pressed(&"interact") or Input.is_action_just_pressed(&"mouse_left"):
+				hud.dialogue_box.continue_dialogue()
 
 func kill() -> void:
 	global_position = spawn_pos
